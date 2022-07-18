e65cacb386a7d2a169d7bbf9e6565d93af57f31f
Kerian Thuillier <kerian.thuillier@irisa.fr>
2022-07-18 11:22:31 +0200

    Workspace structure

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from setuptools import setup
 
-setup(name='clingopt',
-      version='0.0.1',
-      description='clingOPT extends the solver clingo with optimisation theories over reals.',
-      url='https://github.com/kthuillier/MERRIN_Generic',
-      author='Kerian Thuillier',
-      author_email='kerian.thuillier@irisa.fr',
-      license='?',
-      packages=['clingopt'],
-      package_dir={'clingopt': 'src'},
-      zip_safe=False,install_requires=[
-          'pulp',
-          'clingo',
-      ]
-    )
+setup(
+    name='clingopt',
+    version='0.0.1',
+    description='clingOPT extends the solver clingo with optimisation theories over reals.',
+    url='https://github.com/kthuillier/MERRIN_Generic',
+    author='Kerian Thuillier',
+    author_email='kerian.thuillier@irisa.fr',
+    license='?',
+    packages=['clingopt'],
+    package_dir={'clingopt': 'src'},
+    install_requires=[
+        'pulp',
+        'clingo',
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    include_package_data=True,
+    zip_safe=False,
+)
+
